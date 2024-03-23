58d2b4c3e5cbff51508ff33a5c837f94d7c8262c
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-03-22 20:42:33 -0700

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
-#!/usr/bin/pup
 # Install flask
-package { 'flask':
+package{'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
-package { 'Werkzeug':
+package{'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3'
 }
