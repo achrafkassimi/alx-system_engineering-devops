03c91e2347636147e447bd81568e8e759a131997
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-05-07 15:10:34 -0700

    tasks mm 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,9 @@
 Script that queries subscribers on a given Reddit subreddit.
 """
 
-import requests
-
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    import requests
     urls = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(urls, headers=headers, allow_redirects=False)
